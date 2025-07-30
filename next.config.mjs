3ada34110c33636f12c4318b98e9081982af959f
mahmoudsruby <mahmoudsruby@gmail.com>
2025-07-30 06:45:42 +0300

    Add package-lock.json file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+
+    if (isServer) {
+      config.externals.push('lightningcss');
+    }
+    return config;
+  },
+
+};
 
 export default nextConfig;
